513534d65e89da0ee063247b36f97782b911067d
Fernando-Villa <68962694+Fernando-Villa@users.noreply.github.com>
2022-09-13 22:45:10 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp6/Empleado - Copia.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleApp6
+{
+    internal class pro: Item
+    {
+        public String Nombres;
+        public String Direccion;
+        public String Correo;
+        public Double Telefono;
+
+        public pro(Consola _consola, BaseDatos _objBD) : base(_consola, _objBD, "000","Empleado", "Cédula")
+        {          
+        }
+
+        public pro(Consola _consola, BaseDatos _objBD, String _Codigo,
+            String Nombres, String Direccion, String Correo, Double Telefono)
+            :base(_consola, _objBD,_Codigo, "pro", "Cédula")
+        {
+            this.Nombres = Nombres;
+            this.Direccion = Direccion;
+            this.Correo = Correo;
+            this.Telefono = Telefono;
+        }
+
+        public override Item creatItem(Consola _consola, BaseDatos _objBD)
+        {
+            return new pro(_consola, _objBD);
+        }
+
+        public override Item creatItem(Consola _consola, BaseDatos _objBD, DataRow _registro)
+        {
+            return new pro(_consola, _objBD, _registro["Cedula"].ToString(), 
+                _registro["Nombres"].ToString(), _registro["Direccion"].ToString(), _registro["Correo"].ToString(),
+                Double.Parse(_registro["Telefono"].ToString()));
+        }
+
+        public override void mostrarMembreteTabla()
+        {
+            consola.Escribir(40, 2, ConsoleColor.Yellow, "LISTA DE EMPLEADOS");
+            consola.Escribir(5, 5, ConsoleColor.Blue, "N°"); consola.Escribir(10, 5, ConsoleColor.Blue, "Cédula");
+            consola.Escribir(30, 5, ConsoleColor.Blue, "Nombres"); consola.Escribir(60, 5, ConsoleColor.Blue, "Área");
+            consola.Escribir(70, 5, ConsoleColor.Blue, "Cargo"); consola.Escribir(80, 5, ConsoleColor.Blue, "Sueldo");
+            consola.Marco(3, 4, 95, 15);
+        }
+
+        public override void mostrarInfoComoFila(int Num, int fila)
+        {
+            consola.Escribir(5, fila, ConsoleColor.White, Num.ToString()); 
+            consola.Escribir(10, fila, ConsoleColor.White, Codigo);
+            consola.Escribir(25, fila, ConsoleColor.White, Nombres); 
+            consola.Escribir(55, fila, ConsoleColor.White, Direccion);
+            consola.Escribir(70, fila, ConsoleColor.White, Correo);
+            consola.Escribir(80, fila, ConsoleColor.White, Telefono.ToString("0.00"));
+        }
+      
+
+        public override void mostrarInfo()
+        {
+            
+            consola.Escribir(30, 2, ConsoleColor.Red, "INFORMACIÓN DEL EMPLEADO");
+            consola.Marco(10, 3, 65, 11);
+            consola.Escribir(20, 5, ConsoleColor.Yellow, "Cédula: "); consola.Escribir(35, 5, ConsoleColor.White, Codigo);
+            consola.Escribir(20, 6, ConsoleColor.Yellow, "Nombres: "); consola.Escribir(35, 6, ConsoleColor.White, Nombres);
+            consola.Escribir(20, 7, ConsoleColor.Yellow, "Area: "); consola.Escribir(35, 7, ConsoleColor.White, Direccion);
+            consola.Escribir(20, 8, ConsoleColor.Yellow, "Cargo: "); consola.Escribir(35, 8, ConsoleColor.White, Correo);
+            consola.Escribir(20, 8, ConsoleColor.Yellow, "Sueldo: "); consola.Escribir(35, 8, ConsoleColor.White, Telefono.ToString("0.00"));
+        }
+
+        public override void leerInfo()
+        {
+
+            consola.Escribir(30, 2, ConsoleColor.Red, "NUEVO EMPLEADO");
+            consola.Marco(10, 3, 65, 11);
+            consola.Escribir(20, 5, ConsoleColor.Yellow, "Cédula: ");
+            consola.Escribir(20, 6, ConsoleColor.Yellow, "Nombres: ");
+            consola.Escribir(20, 7, ConsoleColor.Yellow, "Area: ");
+            consola.Escribir(20, 8, ConsoleColor.Yellow, "Cargo: ");
+            consola.Escribir(20, 9, ConsoleColor.Yellow, "Sueldo: ");
+            Codigo = consola.leerCadena(35, 5);
+            Nombres = consola.leerCadena(35, 6);
+            Direccion = consola.leerCadena(35, 7);
+            Correo = consola.leerCadena(35, 8);
+            Telefono = Double.Parse(consola.leerCadena(35, 9));
+
+        }
+
+        public override String getSQL(String TipoSQL, String CodigoABuscar = "") 
+        {
+            String SQL="";
+            switch (TipoSQL){
+                case "Insert": 
+                    SQL= "Insert into TbEmpleados (Cedula, Nombres, Area, Cargo, Sueldo) values('"
+                          + Codigo + "','" + Nombres + "','" + Direccion + "','" + Correo + "'," + Telefono +");";
+                    break;
+                case "Delete":
+                    SQL = "Delete from TbEmpleados where Cedula='" + CodigoABuscar + "'";
+                    break;
+                case "Select":
+                    if (CodigoABuscar != "")
+                    {
+                        SQL = "Select * from TbEmpleados where Cedula='" + CodigoABuscar + "'";
+                    }
+                    else
+                    {
+                        SQL = "Select * from TbEmpleados order by Cedula";
+                    }
+                    break;
+              }
+
+            return SQL;
+        }
+
+
+
+        /*   public int actualizarPrecioProductoBD(double nuevoPrecio)
+           {
+               String SQL = "update tbProductos set precio=" + nuevoPrecio.ToString() + " where Codigo='" + Codigo + "'";
+               return objBD.ejecutarComando(SQL);
+           }
+        */
+
+        /*public void actualizarPrecio()
+      {
+          Console.Clear();
+          mostrarInfoProducto();
+          consola.Escribir(20, 10, ConsoleColor.Red, "Nuevo Precio: ");
+          double NuevoPrecio = consola.leerNumeroDecimal(35, 10);
+
+          if (actualizarPrecioProductoBD(NuevoPrecio) > 0)
+          {
+              Precio = NuevoPrecio;
+              consola.Escribir(20, 13, ConsoleColor.Blue, "Precio Actualizado! ");
+          }
+          else
+          {
+              consola.Escribir(20, 13, ConsoleColor.Blue, "Error al Actualizar precio! ");
+          }
+
+           Console.ReadLine();
+      }*/
+
+    }
+}
---
+++ ConsoleApp6/Proveedores.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ConsoleApp6
+{
+    internal class Proveedor: Item
+    {
+        public String ID;
+        public String RUC;
+        public String RazonSocial;
+        public String Telefono;
+        public String Direccion;
+        public String Ciudad;
+
+        public Proveedor(Consola _consola, BaseDatos _objBD) : base(_consola, _objBD, "000","Proveedor", "ID")
+        {          
+        }
+
+        public Proveedor(Consola _consola, BaseDatos _objBD, String _ID,
+            String ID, String RUC, String RazonSocial, String Telefono, String Direccion, String Ciudad)
+            :base(_consola, _objBD,_ID, "Proveedor", "ID")
+        {
+            this.ID = ID;
+            this.RUC = RUC;
+            this.RazonSocial = RazonSocial;
+            this.Telefono = Telefono;
+            this.Direccion = Direccion;
+            this.Ciudad = Ciudad;
+        }
+
+        public override Item creatItem(Consola _consola, BaseDatos _objBD)
+        {
+            return new Proveedor(_consola, _objBD);
+        }
+
+        public override Item creatItem(Consola _consola, BaseDatos _objBD, DataRow _registro)
+        {
+            return new Proveedor(_consola, _objBD, _registro["ID"].ToString(), _registro["RUC"].ToString(), _registro["RazonSocial"].ToString(),
+                _registro["Nombre"].ToString(), _registro["Telefono"].ToString(), _registro["Direccion"].ToString(),
+                _registro["Ciudad"].ToString());
+        }
+
+        public override void mostrarMembreteTabla()
+        {
+            consola.Escribir(40, 2, ConsoleColor.Yellow, "LISTA DE PROVEEDORES");
+            consola.Escribir(5, 5, ConsoleColor.Blue, "N°"); consola.Escribir(10, 5, ConsoleColor.Blue, "ID");
+            consola.Escribir(30, 5, ConsoleColor.Blue, "RUC"); consola.Escribir(60, 5, ConsoleColor.Blue, "Telefono");
+            consola.Escribir(70, 5, ConsoleColor.Blue, "RazonSocial"); consola.Escribir(80, 5, ConsoleColor.Blue, "Telefono");
+            consola.Escribir(90, 5, ConsoleColor.Blue, "Direccion"); consola.Escribir(100, 5, ConsoleColor.Blue, "Ciudad");
+            consola.Marco(3, 4, 95, 15);
+        }
+
+        public override void mostrarInfoComoFila(int Num, int fila)
+        {
+            consola.Escribir(5, fila, ConsoleColor.White, Num.ToString()); 
+            consola.Escribir(10, fila, ConsoleColor.White, ID);
+            consola.Escribir(25, fila, ConsoleColor.White, RUC);
+            consola.Escribir(55, fila, ConsoleColor.White, RazonSocial);
+            consola.Escribir(70, fila, ConsoleColor.White, Telefono);
+            consola.Escribir(80, fila, ConsoleColor.White, Direccion);
+            consola.Escribir(90, fila, ConsoleColor.White, Ciudad);
+        }
+      
+
+        public override void mostrarInfo()
+        {
+            
+            consola.Escribir(30, 2, ConsoleColor.Red, "INFORMACIÓN DEL PROVEEDOR");
+            consola.Marco(10, 3, 65, 11);
+            consola.Escribir(20, 5, ConsoleColor.Yellow, "ID: "); consola.Escribir(35, 5, ConsoleColor.White, ID);
+            consola.Escribir(20, 6, ConsoleColor.Yellow, "RUC: "); consola.Escribir(35, 6, ConsoleColor.White, RUC);
+            consola.Escribir(20, 7, ConsoleColor.Yellow, "RazonSocial: "); consola.Escribir(35, 6, ConsoleColor.White, RazonSocial);
+            consola.Escribir(20, 8, ConsoleColor.Yellow, "Telefono: "); consola.Escribir(35, 7, ConsoleColor.White, Telefono);
+            consola.Escribir(20, 9, ConsoleColor.Yellow, "Direccion: "); consola.Escribir(35, 8, ConsoleColor.White, Direccion);
+            consola.Escribir(20, 10, ConsoleColor.Yellow, "Ciudad: "); consola.Escribir(35, 8, ConsoleColor.White, Ciudad);
+        }
+
+        public override void leerInfo()
+        {
+
+            consola.Escribir(30, 2, ConsoleColor.Red, "NUEVO EMPLEADO");
+            consola.Marco(10, 3, 65, 11);
+            consola.Escribir(20, 5, ConsoleColor.Yellow, "ID: ");
+            consola.Escribir(20, 6, ConsoleColor.Yellow, "RUC: ");
+            consola.Escribir(20, 7, ConsoleColor.Yellow, "RazonSocial: ");
+            consola.Escribir(20, 8, ConsoleColor.Yellow, "Telefono: ");
+            consola.Escribir(20, 9, ConsoleColor.Yellow, "Direccion: ");
+            consola.Escribir(20, 10, ConsoleColor.Yellow, "Ciudad: ");
+            ID = consola.leerCadena(35, 5);
+            RUC = consola.leerCadena(35, 6);
+            RazonSocial = consola.leerCadena(35, 7);
+            Telefono = consola.leerCadena(35, 8);
+            Direccion = consola.leerCadena(35, 9);
+            Ciudad = consola.leerCadena(35, 10);
+
+        }
+
+        public override String getSQL(String TipoSQL, String CodigoABuscar = "") 
+        {
+            String SQL="";
+            switch (TipoSQL){
+                case "Insert": 
+                    SQL= "Insert into TbProveedores (ID, RUC, RazonSocial, Telefono, Direccion, Ciudad) values('"
+                          + ID + "','" + RUC + "','" + RazonSocial + "','" + Telefono + "','" + Direccion + "','" + Ciudad + ");";
+                    break;
+                case "Delete":
+                    SQL = "Delete from TbProveedores where ID='" + CodigoABuscar + "'";
+                    break;
+                case "Select":
+                    if (CodigoABuscar != "")
+                    {
+                        SQL = "Select * from TbProveedores where ID='" + CodigoABuscar + "'";
+                    }
+                    else
+                    {
+                        SQL = "Select * from TbProveedores order by codigo";
+                    }
+                    break;
+              }
+
+            return SQL;
+        }
+    }
+}
